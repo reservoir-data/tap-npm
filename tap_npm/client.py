d8df49eabd4d360834d47b9660fddfc166583094
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-10-02 13:23:44 -0500

    add PK

--- tap_npm/client.py
+++ tap_npm/client.py
@@ -14,6 +14,7 @@ class NPMPackageStream(RESTStream):
 
     url_base = "https://registry.npmjs.org"
     name = "packages"
+    primary_keys = ["_id"]
     schema_filepath = SCHEMAS_DIR / "packages.json"
     records_jsonpath = "$"
     path = "/{package}"
